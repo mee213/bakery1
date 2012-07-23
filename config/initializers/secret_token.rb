a9d112893bc22bed4581fa07eecdfb6fd77c90c1
Marlo Evans <evans.marlo@gmail.com>
2012-07-22 22:28:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bakery1::Application.config.secret_token = 'c16156973a46c9e651d3d13d2f74c1cf845952d16316528eda7aaf430327cd579a17debc40dc229d894ec2666090e9bb2a374958c25f850a601ba31f52ca6b00'
